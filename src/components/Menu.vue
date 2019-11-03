d836abd78ccc01de59889513a90d443d3200991e
Ryan Faulhaber <faulhaberryan@gmail.com>
2019-11-03 18:20:43 -0500

    some nonsense added

---
+++ src/components/Menu.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div>
+      <Roller v-bind:currentCharacter="character" />
+    </div>
+    <ul>
+      <li>
+        <a>Character creation</a>
+      </li>
+      <li>
+        <a>Character sheet</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Roller from "./Roller.vue";
+
+export default {
+  name: "Menu",
+  components: {
+    Roller
+  },
+  data: function() {
+    return {
+      character: {
+        name: "foo"
+      }
+    };
+  }
+};
+</script>
---
+++ src/components/Roller.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="button-box">
+    <button>1D</button>
+    <button>2D</button>
+
+    <div v-if="currentCharacter">your name is: {{currentCharacter.name}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Roller",
+  props: {
+    currentCharacter: Object
+  }
+};
+</script>
+
+<style scoped>
+.button-box {
+  border: 1px solid black;
+}
+</style>
